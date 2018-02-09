bacb26965d083ed940dca95805bcc1a129b1a477
jackmaloney <jack.maloney@transformuk.com>
2018-02-09 16:29:47 +0000

    SSCS-2576 Send Notification to gov notify
    - Add missing fields to allow an appeal received email to be sent to Gov Notify
    - Hardcoded values which are dependent on other stories and added TODOs

--- src/main/java/uk/gov/hmcts/sscs/placeholders/AppealReceivedPersonalisation.java
+++ src/main/java/uk/gov/hmcts/sscs/placeholders/AppealReceivedPersonalisation.java
@@ -1,7 +1,11 @@
 package uk.gov.hmcts.sscs.placeholders;
 
 import static com.google.common.collect.Maps.newHashMap;
+import static java.time.temporal.ChronoUnit.DAYS;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Map;
 import uk.gov.hmcts.sscs.config.NotificationConfig;
 import uk.gov.hmcts.sscs.domain.CcdResponse;
@@ -13,8 +17,11 @@ public class AppealReceivedPersonalisation extends Personalisation {
 
     public static final String FIRST_TIER_AGENCY_ACRONYM = "first_tier_agency_acronym";
     public static final String FIRST_TIER_AGENCY_FULL_NAME = "first_tier_agency_full_name";
+    private static final String APPEAL_RESPOND_DATE = "appeal_respond_date";
     public static final String DWP_ACRONYM = "DWP";
     public static final String DWP_FUL_NAME = "Department for Work and Pensions";
+    private static final int MAX_DWP_RESPONSE_DAYS = 35;
+    private static final String RESPONSE_DATE_FORMAT = "dd MMMM yyyy";
 
     public AppealReceivedPersonalisation(NotificationConfig config) {
         super(config);
@@ -25,6 +32,12 @@ public class AppealReceivedPersonalisation extends Personalisation {
         Map<String, String> personalisation = newHashMap(defaultMap);
         personalisation.put(FIRST_TIER_AGENCY_ACRONYM, DWP_ACRONYM);
         personalisation.put(FIRST_TIER_AGENCY_FULL_NAME, DWP_FUL_NAME);
+
+        // TODO: Set this to the actual event date once event story has been implemented
+        ZonedDateTime z = ZonedDateTime.of(1900, 1, 1, 0, 0, 0, 0, ZoneId.of("GMT"));
+
+        String dwpResponseDateString = z.plus(MAX_DWP_RESPONSE_DAYS, DAYS).format(DateTimeFormatter.ofPattern(RESPONSE_DATE_FORMAT));
+        personalisation.put(APPEAL_RESPOND_DATE, dwpResponseDateString);
 
         return personalisation;
     }
