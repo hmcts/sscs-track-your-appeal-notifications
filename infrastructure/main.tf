2cab0948e10d68b50de7c8b1adebcc0eab06d2c7
Sathish Jeganathan <Satish.Jaganathan@HMCTS.NET>
2018-02-12 12:01:41 +0000

    SSCS-2694 Added test notification key

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,3 +1,19 @@
+provider "vault" {
+  //  # It is strongly recommended to configure this provider through the
+  //  # environment variables described above, so that each user can have
+  //  # separate credentials set in the environment.
+  //  #
+  //  # This will default to using $VAULT_ADDR
+  //  # But can be set explicitly
+  address = "https://vault.reform.hmcts.net:6200"
+}
+
+data "vault_generic_secret" "sscs_notify_api_key" {
+  path = "secret/test/sscs/sscs_notify_api_new_key"
+}
+
+
+
 module "track-your-appeal-notifications" {
   source   = "git@github.com:contino/moj-module-webapp?ref=master"
   product  = "${var.product}-notifications"
@@ -7,5 +23,6 @@ module "track-your-appeal-notifications" {
 
   app_settings = {
     MANAGEMENT_SECURITY_ENABLED = "${var.management_security_enabled}"
+    NOTIFICATION_API_KEY = "${data.vault_generic_secret.sscs_notify_api_key.data["value"]}"
   }
 }
