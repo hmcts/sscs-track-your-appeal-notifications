a49af5d9c6707877e027a5c2a47f2fe80a543f5a
jackmaloney <jack.maloney@transformuk.com>
2018-02-27 11:04:20 +0000

    Improve logging for Notification Service

--- src/main/java/uk/gov/hmcts/sscs/service/AuthorisationService.java
+++ src/main/java/uk/gov/hmcts/sscs/service/AuthorisationService.java
@@ -1,5 +1,7 @@
 package uk.gov.hmcts.sscs.service;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 import feign.FeignException;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.authorisation.ServiceAuthorisationApi;
@@ -8,6 +10,7 @@ import uk.gov.hmcts.sscs.exception.AuthorisationException;
 @Service
 public class AuthorisationService {
 
+    private static final org.slf4j.Logger LOG = getLogger(AuthorisationService.class);
     public static final String SERVICE_AUTHORISATION_HEADER = "ServiceAuthorization";
 
     private final ServiceAuthorisationApi serviceAuthorisationApi;
@@ -18,10 +21,14 @@ public class AuthorisationService {
 
     public Boolean authorise(String serviceAuthHeader) {
         try {
+            LOG.info("About to authorise Notification request");
             serviceAuthorisationApi.getServiceName(serviceAuthHeader);
+            LOG.info("Notification request authorised");
             return true;
         } catch (FeignException exc) {
             boolean isClientError = exc.status() >= 400 && exc.status() <= 499;
+
+            LOG.error("Authorisation failed for Notification request with status: " + exc.status(), exc);
 
             throw isClientError ? new AuthorisationException(exc.getMessage(), exc) : exc;
         }
