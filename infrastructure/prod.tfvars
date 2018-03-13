f79f91739a3905350bacf3077ed84c6c31d8172b
Sathish Jeganathan <Satish.Jaganathan@HMCTS.NET>
2018-03-13 16:03:00 +0000

    SSCS-2741 Changed the TYA urls in the notification to the following formats
    https://track-appeal.nonprod.platform.hmcts.net/evidence/<appeal_number>
    
    https://track-appeal.nonprod.platform.hmcts.net/manage-email-notifications/<mac_id>
    
    https://track-appeal.nonprod.platform.hmcts.net/trackyourappeal/<appeal_number>
    
    https://track-appeal.nonprod.platform.hmcts.net/abouthearing/<appeal_number>
    
    https://track-appeal.nonprod.platform.hmcts.net/expenses/<appeal_number>
    :

--- infrastructure/prod.tfvars
+++ infrastructure/prod.tfvars
@@ -1,1 +1,7 @@
 infrastructure_env = "prod"
+
+evidence_submission_info_link = "https://track-appeal.platform.hmcts.net/evidence/appeal_id"
+sscs_manage_emails_link = "https://track-appeal.platform.hmcts.net/manage-email-notifications/mac"
+sscs_track_your_appeal_link = "https://track-appeal.platform.hmcts.net/trackyourappeal/appeal_id"
+hearing_info_link = "https://track-appeal.platform.hmcts.net/abouthearing/appeal_id"
+claiming_expenses_link = "https://track-appeal.platform.hmcts.net/expenses/appeal_id"
---
+++ src/IntegrationTests/java/uk/gov/hmcts/sscs/tya/NotificationsTyaLinksIt.java
@@ -0,0 +1,35 @@
+package uk.gov.hmcts.sscs.tya;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NotificationsTyaLinksIt {
+
+    @Value("${manage.emails.link}")
+    private String manageEmailsLink;
+    @Value("${track.appeal.link}")
+    private String trackAppealLink;
+    @Value("${evidence.submission.info.link}")
+    private String evidenceSubmissionInfoLink;
+    @Value("${claiming.expenses.link}")
+    private String claimingExpensesLink;
+    @Value("${hearing.info.link}")
+    private String hearingInfoLink;
+
+    @Test
+    public void shouldVerifyTyaLinksAreInValidFormat() {
+        assertThat(manageEmailsLink.endsWith("/manage-email-notifications/mac"), equalTo(true));
+        assertThat(trackAppealLink.endsWith("/trackyourappeal/appeal_id"), equalTo(true));
+        assertThat(evidenceSubmissionInfoLink.endsWith("/evidence/appeal_id"), equalTo(true));
+        assertThat(claimingExpensesLink.endsWith("/expenses/appeal_id"), equalTo(true));
+        assertThat(hearingInfoLink.endsWith("/abouthearing/appeal_id"), equalTo(true));
+    }
+}
