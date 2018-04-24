61518c2245137c5aca10980d7b06a112735dc5ff
jackmaloney <jack.maloney@transformuk.com>
2018-04-24 14:27:29 +0100

    Add Jsoninclude to CCD response

--- src/main/java/uk/gov/hmcts/sscs/domain/CcdResponse.java
+++ src/main/java/uk/gov/hmcts/sscs/domain/CcdResponse.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.sscs.domain;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import java.util.List;
 import lombok.Builder;
 import lombok.Data;
@@ -8,6 +9,7 @@ import uk.gov.hmcts.sscs.domain.notify.EventType;
 
 @Data
 @Builder(toBuilder = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class CcdResponse {
 
     private String caseId;
