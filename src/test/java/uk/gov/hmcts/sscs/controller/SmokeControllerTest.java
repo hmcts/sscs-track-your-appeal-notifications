079300c66b517bbf452579f3d6a15534e3479369
jackmaloney <jack.maloney@transformuk.com>
2018-04-26 14:09:29 +0100

    Add missing mock beans to smoke controller test

--- src/test/java/uk/gov/hmcts/sscs/controller/SmokeControllerTest.java
+++ src/test/java/uk/gov/hmcts/sscs/controller/SmokeControllerTest.java
@@ -9,14 +9,21 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import uk.gov.hmcts.sscs.service.ccd.SearchCcdService;
+import uk.gov.hmcts.sscs.service.idam.IdamService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class SmokeControllerTest {
 
+    @MockBean
+    private SearchCcdService searchCcdService;
+    @MockBean
+    private IdamService idamService;
     @Autowired
     private MockMvc mockMvc;
 
