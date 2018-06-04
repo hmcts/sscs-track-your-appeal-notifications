f7f6bc3bd40bada3d9455d9a5569067bea47c658
jackmaloney <jack.maloney@transformuk.com>
2018-06-04 11:10:57 +0100

    Fix broken action executor test

--- src/test/java/uk/gov/hmcts/sscs/service/scheduler/ActionExecutorTest.java
+++ src/test/java/uk/gov/hmcts/sscs/service/scheduler/ActionExecutorTest.java
@@ -1,18 +1,17 @@
 package uk.gov.hmcts.sscs.service.scheduler;
 
-import static org.junit.Assert.*;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.eq;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.mockito.MockitoAnnotations.initMocks;
+import static uk.gov.hmcts.sscs.domain.notify.EventType.EVIDENCE_REMINDER;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.sscs.deserialize.CcdResponseWrapperDeserializer;
+import uk.gov.hmcts.sscs.domain.CcdResponse;
 import uk.gov.hmcts.sscs.domain.CcdResponseWrapper;
 import uk.gov.hmcts.sscs.service.NotificationService;
 import uk.gov.hmcts.sscs.service.ccd.SearchCcdService;
@@ -20,8 +19,6 @@ import uk.gov.hmcts.sscs.service.ccd.UpdateCcdService;
 import uk.gov.hmcts.sscs.service.idam.IdamService;
 
 public class ActionExecutorTest {
-
-    //TODO: Implement this
 
     private ActionExecutor actionExecutor;
 
@@ -38,6 +35,7 @@ public class ActionExecutorTest {
 
     private CaseDetails caseDetails;
     private CcdResponseWrapper wrapper;
+    private CcdResponse newCcdResponse;
 
     @Before
     public void setup() {
@@ -47,15 +45,18 @@ public class ActionExecutorTest {
 
         caseDetails = CaseDetails.builder().caseTypeId("123").build();
 
-        wrapper = null;
+        newCcdResponse = CcdResponse.builder().notificationType(EVIDENCE_REMINDER).build();
+
+        wrapper = CcdResponseWrapper.builder().newCcdResponse(newCcdResponse).build();
     }
 
     @Test
     public void givenAReminderIsTriggered_thenActionExecutorShouldProcessTheJob() {
-        actionExecutor.execute("1", "jobName", "123456");
-//
         when(searchCcdService.getByCaseId(eq("123456"), any())).thenReturn(caseDetails);
         when(deserializer.buildCcdResponseWrapper(any())).thenReturn(wrapper);
+        when(updateCcdService.update(eq(newCcdResponse), eq(123456L), eq(EVIDENCE_REMINDER.getId()), any())).thenReturn(caseDetails);
+
+        actionExecutor.execute("1", "jobName", "123456");
 
         verify(notificationService, times(1)).createAndSendNotification(wrapper);
         verify(updateCcdService, times(1)).update(any(), any(), any(), any());
