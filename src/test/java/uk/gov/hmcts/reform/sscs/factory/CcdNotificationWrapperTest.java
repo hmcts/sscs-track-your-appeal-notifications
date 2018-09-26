77054a2277a090bfcb35e1b3efe1d7ac7ab957e2
david.crespo <david.crespo@hmcts.net>
2018-09-26 12:40:18 +0200

    Fix ONLINE unit test and parametrise all of them to reuse code

--- src/test/java/uk/gov/hmcts/reform/sscs/factory/CcdNotificationWrapperTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/factory/CcdNotificationWrapperTest.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.reform.sscs.factory;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
-import static uk.gov.hmcts.reform.sscs.config.AppealHearingType.ONLINE;
 
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
@@ -16,32 +15,27 @@ import uk.gov.hmcts.reform.sscs.domain.SscsCaseDataWrapper;
 
 @RunWith(JUnitParamsRunner.class)
 public class CcdNotificationWrapperTest {
-    @Test
-    public void hearingTypeIsOnline() {
-        AppealHearingType hearingType = new CcdNotificationWrapper(SscsCaseDataWrapper.builder()
-                .newSscsCaseData(SscsCaseData.builder()
-                        .onlinePanel(OnlinePanel.builder().build())
-                        .build())
-                .build()).getHearingType();
 
-        assertThat(hearingType, is(ONLINE));
+    @Test
+    @Parameters({"paper, PAPER", "null, REGULAR", "oral, ONLINE"})
+    public void should_returnAppealHearingType_when_hearingType(String hearingType,
+                                                                AppealHearingType expected) {
+        CcdNotificationWrapper ccdNotificationWrapper = buildCcdNotificationWrapper(hearingType);
+
+        assertThat(ccdNotificationWrapper.getHearingType(), is(expected));
     }
 
-    @Test
-    public void hearingTypeIsOral() {
-        AppealHearingType hearingType = new CcdNotificationWrapper(SscsCaseDataWrapper.builder()
-                .newSscsCaseData(SscsCaseData.builder()
-                        .build())
-                .build()).getHearingType();
-
-        assertThat(hearingType, is(AppealHearingType.REGULAR));
-    }
-
-    @Test
-    @Parameters({"paper, PAPER", "null, REGULAR"})
-    public void should_returnAppealHearingTypePaper_when_hearingTypeIsPaper(String hearingType,
-                                                                            AppealHearingType expected) {
-        CcdNotificationWrapper ccdNotificationWrapper = new CcdNotificationWrapper(
+    private CcdNotificationWrapper buildCcdNotificationWrapper(String hearingType) {
+        if ("oral".equals(hearingType)) {
+            return new CcdNotificationWrapper(
+                    SscsCaseDataWrapper.builder()
+                            .newSscsCaseData(SscsCaseData.builder()
+                                    .onlinePanel(OnlinePanel.builder().build())
+                                    .build())
+                            .build()
+            );
+        }
+        return new CcdNotificationWrapper(
                 SscsCaseDataWrapper.builder()
                         .newSscsCaseData(SscsCaseData.builder()
                                 .appeal(Appeal.builder()
@@ -50,7 +44,5 @@ public class CcdNotificationWrapperTest {
                                 .build())
                         .build()
         );
-
-        assertThat(ccdNotificationWrapper.getHearingType(), is(expected));
     }
 }
