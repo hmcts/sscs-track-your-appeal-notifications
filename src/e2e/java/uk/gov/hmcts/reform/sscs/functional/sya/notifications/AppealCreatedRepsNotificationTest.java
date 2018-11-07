3dd82b1d1785a67c8a17ba374a39ee49efd1468a
david.crespo <david.crespo@hmcts.net>
2018-11-07 01:49:06 +0100

    Add functional test to cover appealCreated for reps subscription
    Upgrade minor common version
    Minor refactoring on functional test

--- src/e2e/java/uk/gov/hmcts/reform/sscs/functional/sya/notifications/AppealCreatedRepsNotificationTest.java
+++ src/e2e/java/uk/gov/hmcts/reform/sscs/functional/sya/notifications/AppealCreatedRepsNotificationTest.java
@@ -1,15 +1,40 @@
 package uk.gov.hmcts.reform.sscs.functional.sya.notifications;
 
+import static uk.gov.hmcts.reform.sscs.domain.notify.NotificationEventType.SYA_APPEAL_CREATED_NOTIFICATION;
+
+import java.util.List;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Value;
 import uk.gov.hmcts.reform.sscs.functional.AbstractFunctionalTest;
+import uk.gov.service.notify.Notification;
 
 public class AppealCreatedRepsNotificationTest extends AbstractFunctionalTest {
+    @Value("${notification.appealCreated.appellant.emailId}")
+    private String appealCreatedAppellantEmailId;
+    @Value("${notification.appealCreated.appellant.smsId}")
+    private String appealCreatedAppellantSmsId;
+    @Value("${notification.appealCreated.representative.emailId}")
+    private String appealCreatedRepsEmailId;
+    @Value("${notification.appealCreated.representative.smsId}")
+    private String appealCreatedRepsSmsId;
+
     public AppealCreatedRepsNotificationTest() {
         super(30);
     }
 
     @Test
-    public void givenAppealCreatedEventAndRepsSubscription_shouldSendAppealCreatedNotificationToReps() {
+    public void givenAppealCreatedEventAndRepsSubscription_shouldSendAppealCreatedNotificationToReps()
+            throws Exception {
+        simulateCcdCallback(SYA_APPEAL_CREATED_NOTIFICATION,
+                "representative/" + SYA_APPEAL_CREATED_NOTIFICATION.getId() + "Callback.json");
 
+        List<Notification> notifications = tryFetchNotificationsForTestCase(
+                appealCreatedAppellantEmailId, appealCreatedAppellantSmsId,
+                appealCreatedRepsEmailId, appealCreatedRepsSmsId);
+
+        assertNotificationBodyContains(notifications, appealCreatedAppellantEmailId);
+        assertNotificationBodyContains(notifications, appealCreatedAppellantSmsId);
+        assertNotificationBodyContains(notifications, appealCreatedRepsEmailId);
+        assertNotificationBodyContains(notifications, appealCreatedRepsSmsId);
     }
 }
--- src/e2e/java/uk/gov/hmcts/reform/sscs/functional/sya/notifications/AppealDormantNotificationTest.java
+++ src/e2e/java/uk/gov/hmcts/reform/sscs/functional/sya/notifications/AppealDormantNotificationTest.java
@@ -15,6 +15,7 @@ import uk.gov.service.notify.NotificationClientException;
 
 public class AppealDormantNotificationTest extends AbstractFunctionalTest {
 
+    private static final String PAPER_APPEAL_DORMANT_PATH = "paper/appealDormant/";
     @Value("${notification.paper.appealDormant.emailId}")
     private String paperAppealDormantEmailId;
 
@@ -27,8 +28,9 @@ public class AppealDormantNotificationTest extends AbstractFunctionalTest {
 
     @Test
     public void shouldSendPaperAppealDormantNotification() throws Exception {
-        simulateCcdCallback(APPEAL_DORMANT_NOTIFICATION, "paper/appealDormant/paper-" + APPEAL_DORMANT_NOTIFICATION.getId()
-                + "Callback.json");
+        simulateCcdCallback(APPEAL_DORMANT_NOTIFICATION,
+                PAPER_APPEAL_DORMANT_PATH + "paper-" + APPEAL_DORMANT_NOTIFICATION.getId()
+                        + "Callback.json");
         List<Notification> notifications = tryFetchNotificationsForTestCase(
                 paperAppealDormantEmailId, paperAppealDormantSmsId);
         assertEmailAndSmsNotificationContentIsAsExpected(notifications);
@@ -49,7 +51,7 @@ public class AppealDormantNotificationTest extends AbstractFunctionalTest {
 
     @Test
     public void shouldNotSendPaperAppealDormantdNotificationIfNotSubscribed() throws NotificationClientException, IOException {
-        simulateCcdCallback(APPEAL_DORMANT_NOTIFICATION, "paper/appealDormant/paper-no-subscriptions-"
+        simulateCcdCallback(APPEAL_DORMANT_NOTIFICATION, PAPER_APPEAL_DORMANT_PATH + "paper-no-subscriptions-"
                 + APPEAL_DORMANT_NOTIFICATION.getId()
                 + "Callback.json");
         List<Notification> notifications = tryFetchNotificationsForTestCaseWithFlag(true,
