6036b30d4d9cf6805c1b18d0bdd41713ea3b627e
Oleg <oleg.proudnikov@hmcts.net>
2019-01-03 15:01:46 +0000

    SSCS-4849 Send SMS to UK mobiles only

---
+++ src/main/java/uk/gov/hmcts/reform/sscs/domain/util/UkMobile.java
@@ -0,0 +1,17 @@
+package uk.gov.hmcts.reform.sscs.domain.util;
+
+import java.util.regex.Pattern;
+
+public class UkMobile {
+
+    private static final Pattern UK_MOBILE = Pattern.compile("(?:(?:(\\+44[\\s]?)|(?:\\(?0\\)?)[\\s]?)(?:\\d[\\s]?){10})");
+
+    private UkMobile() {
+        // Void
+    }
+
+    public static boolean check(String value) {
+        return value != null && UK_MOBILE.matcher(value).matches();
+    }
+
+}
