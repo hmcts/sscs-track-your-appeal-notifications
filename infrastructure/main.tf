e73da1c8355b07fdfc7e6890e28247e87f781415
david.crespo <david.crespo@hmcts.net>
2019-01-21 18:16:49 +0000

    move properties above to make sure they below to the default profile

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -68,7 +68,7 @@ locals {
   cohApi    = "http://coh-cor-${local.local_env}.service.${local.local_ase}.internal"
   documentStore = "http://dm-store-${local.local_env}.service.${local.local_ase}.internal"
 
-  azureVaultName              = "sscs-${local.local_env}"
+  azureVaultName = "sscs-${local.local_env}"
 }
 
 module "track-your-appeal-notifications" {
