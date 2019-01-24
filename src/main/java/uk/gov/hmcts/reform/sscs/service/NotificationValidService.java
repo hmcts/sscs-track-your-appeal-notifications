d81c3f2d5aa2f5590f33150d8cbcc126bec093ed
Mark Hunter <mark.hunter@agilesphere.co.uk>
2019-01-24 09:13:18 +0000

    SSCS-4307 - Cater for additional fallback subscription types

--- src/main/java/uk/gov/hmcts/reform/sscs/service/NotificationValidService.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/service/NotificationValidService.java
@@ -5,8 +5,10 @@ import static uk.gov.hmcts.reform.sscs.config.SubscriptionType.APPOINTEE;
 import static uk.gov.hmcts.reform.sscs.config.SubscriptionType.REPRESENTATIVE;
 import static uk.gov.hmcts.reform.sscs.domain.notify.NotificationEventType.INTERLOC_VALID_APPEAL;
 import static uk.gov.hmcts.reform.sscs.domain.notify.NotificationEventType.STRUCK_OUT;
+import static uk.gov.hmcts.reform.sscs.domain.notify.NotificationEventType.SYA_APPEAL_CREATED_NOTIFICATION;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.List;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.sscs.ccd.domain.Hearing;
@@ -17,7 +19,7 @@ import uk.gov.hmcts.reform.sscs.factory.NotificationWrapper;
 
 @Service
 public class NotificationValidService {
-
+    private static final List<NotificationEventType> FALLBACK_SUBSCRIPTION_TYPES = Arrays.asList(INTERLOC_VALID_APPEAL, SYA_APPEAL_CREATED_NOTIFICATION);
     private static final String HEARING_TYPE_ONLINE_RESOLUTION = "cor";
 
     static final boolean isMandatoryLetter(NotificationEventType eventType) {
@@ -27,7 +29,7 @@ public class NotificationValidService {
     static final boolean isFallbackLetterRequiredForSubscriptionType(NotificationWrapper wrapper, SubscriptionType subscriptionType, NotificationEventType eventType) {
         boolean result = false;
 
-        if (INTERLOC_VALID_APPEAL.equals(eventType)
+        if (FALLBACK_SUBSCRIPTION_TYPES.contains(eventType)
             && (APPELLANT.equals(subscriptionType)
             || APPOINTEE.equals(subscriptionType)
             || (REPRESENTATIVE.equals(subscriptionType) && null != wrapper.getNewSscsCaseData().getAppeal().getRep()))) {
