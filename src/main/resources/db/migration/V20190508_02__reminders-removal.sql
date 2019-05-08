8f182671597dbd9dfe3d07a403135c57560974ac
srochani <sunil.rochani@hotmail.com>
2019-05-08 15:21:40 +0100

    SSCS-5321, SSCS-5322, SSCS-5323, SSCS-5325, SSCS-5326, SSCS-5327, SSCS-5338, SSCS-5339, SSCS-5340, SSCS-5342, SSCS-5343, SSCS-5344 Template changes

--- src/main/resources/db/migration/V20190508_02__reminders-removal.sql
+++ src/main/resources/db/migration/V20190508_02__reminders-removal.sql
@@ -1,8 +1,8 @@
-﻿delete from qrtz_simple_triggers where trigger_name in (select trigger_name from qrtz_triggers where job_group like '%hearingHoldingReminder');
+﻿delete from qrtz_simple_triggers where trigger_name in (select trigger_name from qrtz_triggers where job_group like '%earingHoldingReminder');
 
-delete from qrtz_triggers where job_group like '%hearingHoldingReminder';
+delete from qrtz_triggers where job_group like '%earingHoldingReminder';
 
-delete from qrtz_job_details where job_group like '%hearingHoldingReminder';
+delete from qrtz_job_details where job_group like '%earingHoldingReminder';
 
 delete from qrtz_simple_triggers where trigger_name in (select trigger_name from qrtz_triggers where job_group like '%dwpResponseLateReminder');
 
