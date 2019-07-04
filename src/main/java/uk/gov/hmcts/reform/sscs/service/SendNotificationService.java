3d20294b9f2e0e66a131f2fd5de5e860bcd2a05e
srochani <sunil.rochani@hotmail.com>
2019-07-04 15:43:08 +0100

    SSCS-6052 - Letters – Interloc letters not having name when being sent out (#563)
    * SSCS-6052 - Letters – Interloc letters not having name when being sent out
    
    * SSCS-6052 - Letters – Interloc letters not having name when being sent out
    
    * SSCS-6052 - Letters – Interloc letters not having name when being sent out

--- src/main/java/uk/gov/hmcts/reform/sscs/service/SendNotificationService.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/service/SendNotificationService.java
@@ -4,7 +4,6 @@ import static uk.gov.hmcts.reform.sscs.config.AppConstants.*;
 import static uk.gov.hmcts.reform.sscs.domain.notify.NotificationEventType.*;
 import static uk.gov.hmcts.reform.sscs.service.LetterUtils.*;
 import static uk.gov.hmcts.reform.sscs.service.NotificationUtils.*;
-import static uk.gov.hmcts.reform.sscs.service.NotificationUtils.hasAppointee;
 import static uk.gov.hmcts.reform.sscs.service.NotificationUtils.isOkToSendEmailNotification;
 import static uk.gov.hmcts.reform.sscs.service.NotificationUtils.isOkToSendSmsNotification;
 import static uk.gov.hmcts.reform.sscs.service.NotificationValidService.INTERLOC_LETTERS;
@@ -128,7 +127,7 @@ public class SendNotificationService {
             NotificationHandler.SendNotification sendNotification = () -> {
                 Address addressToUse = getAddressToUseForLetter(wrapper, subscriptionType);
 
-                sendLetterNotificationToAddress(wrapper, notification, addressToUse);
+                sendLetterNotificationToAddress(wrapper, notification, addressToUse, subscriptionType);
             };
 
             if (bundledLettersOn && isBundledLetter(wrapper.getNotificationType())) {
@@ -144,7 +143,7 @@ public class SendNotificationService {
             NotificationHandler.SendNotification sendNotification = () -> {
                 Address addressToUse = getAddressToUseForLetter(wrapper, subscriptionWithType.getSubscriptionType());
 
-                sendLetterNotificationToAddress(wrapper, notification, addressToUse);
+                sendLetterNotificationToAddress(wrapper, notification, addressToUse, subscriptionWithType.getSubscriptionType());
             };
 
             if (bundledLettersOn && isBundledLetter(wrapper.getNotificationType())) {
@@ -156,16 +155,22 @@ public class SendNotificationService {
     }
 
     public static String getRepSalutation(Representative rep) {
-        if (null == rep.getName()
-                || null == rep.getName().getFirstName()
-                || null == rep.getName().getLastName()) {
+        if (null == rep.getName() || null == rep.getName().getFirstName() || null == rep.getName().getLastName()) {
             return REP_SALUTATION;
         } else {
             return rep.getName().getFullNameNoTitle();
         }
     }
 
-    protected void sendLetterNotificationToAddress(NotificationWrapper wrapper, Notification notification, final Address addressToUse) throws NotificationClientException {
+    public static String getRepSalutation(Name name) {
+        if (null == name || null == name.getFirstName() || null == name.getLastName()) {
+            return REP_SALUTATION;
+        } else {
+            return name.getFullNameNoTitle();
+        }
+    }
+
+    protected void sendLetterNotificationToAddress(NotificationWrapper wrapper, Notification notification, final Address addressToUse, SubscriptionType subscriptionType) throws NotificationClientException {
         if (isValidLetterAddress(addressToUse)) {
             Map<String, String> placeholders = notification.getPlaceholders();
             placeholders.put(ADDRESS_LINE_1, addressToUse.getLine1());
@@ -173,15 +178,19 @@ public class SendNotificationService {
             placeholders.put(ADDRESS_LINE_3, addressToUse.getTown() == null ? " " : addressToUse.getTown());
             placeholders.put(ADDRESS_LINE_4, addressToUse.getCounty() == null ? " " : addressToUse.getCounty());
             placeholders.put(POSTCODE_LITERAL, addressToUse.getPostcode());
-            if (hasRepresentative(wrapper.getSscsCaseDataWrapper())) {
-                String repSalutation = getRepSalutation(wrapper.getNewSscsCaseData().getAppeal().getRep());
-                placeholders.put(REPRESENTATIVE_NAME, repSalutation);
+
+            Name nameToUse = getNameToUseForLetter(wrapper, subscriptionType);
+
+            String fullNameNoTitle = (nameToUse == null) ? getRepSalutation(nameToUse) : nameToUse.getFullNameNoTitle();
+
+            placeholders.put(NAME, fullNameNoTitle);
+            if (SubscriptionType.REPRESENTATIVE.equals(subscriptionType)) {
+                placeholders.put(REPRESENTATIVE_NAME, fullNameNoTitle);
+                placeholders.put(APPELLANT_NAME, wrapper.getNewSscsCaseData().getAppeal().getAppellant().getName().getFullNameNoTitle());
             }
 
-            if (hasAppointee(wrapper.getSscsCaseDataWrapper())) {
-                placeholders.put(APPOINTEE_NAME, wrapper.getNewSscsCaseData().getAppeal().getAppellant().getAppointee().getName().getFullNameNoTitle());
-                placeholders.put(CLAIMANT_NAME, wrapper.getNewSscsCaseData().getAppeal().getAppellant().getName().getFullNameNoTitle());
-            }
+            placeholders.put(CLAIMANT_NAME, wrapper.getNewSscsCaseData().getAppeal().getAppellant().getName().getFullNameNoTitle());
+
 
             if (!placeholders.containsKey(APPEAL_RESPOND_DATE)) {
                 ZonedDateTime appealReceivedDate = ZonedDateTime.now().plusSeconds(delay);
