23d325db6eeaaa118616b76259c139baa4a7f64c
Ryan Da Costa <ryan.dacosta@hmcts.net>
2019-09-24 16:53:36 +0100

    SSCS-5210 AddNotificationsSubscription

---
+++ src/test/java/uk/gov/hmcts/reform/sscs/callback/handlers/FilterNotificationsEventsHandlerTest.java
@@ -0,0 +1,58 @@
+package uk.gov.hmcts.reform.sscs.callback.handlers;
+
+import static org.junit.Assert.assertFalse;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import uk.gov.hmcts.reform.sscs.domain.SscsCaseDataWrapper;
+import uk.gov.hmcts.reform.sscs.domain.notify.NotificationEventType;
+import uk.gov.hmcts.reform.sscs.factory.CcdNotificationWrapper;
+import uk.gov.hmcts.reform.sscs.service.NotificationService;
+
+@RunWith(JUnitParamsRunner.class)
+public class FilterNotificationsEventsHandlerTest {
+
+    @Rule
+    public MockitoRule rule = MockitoJUnit.rule();
+
+    @Mock
+    private NotificationService notificationService;
+    private FilterNotificationsEventsHandler handler;
+
+    @Before
+    public void setUp() {
+        handler = new FilterNotificationsEventsHandler(notificationService);
+    }
+
+    @Test
+    @Parameters({"DWP_UPLOAD_RESPONSE"})
+    public void willHandleEvents(NotificationEventType notificationEventType) {
+        SscsCaseDataWrapper callback = SscsCaseDataWrapper.builder().notificationEventType(notificationEventType).build();
+        handler.handle(callback);
+        verify(notificationService).manageNotificationAndSubscription(eq(new CcdNotificationWrapper(callback)));
+    }
+
+    @Test
+    @Parameters({"ADJOURNED_NOTIFICATION", "DIRECTION_ISSUED"})
+    public void willNotHandleEvents(NotificationEventType notificationEventType) {
+        SscsCaseDataWrapper callback = SscsCaseDataWrapper.builder().notificationEventType(notificationEventType).build();
+        assertFalse(handler.canHandle(callback));
+    }
+
+    @Test(expected = IllegalStateException.class)
+    @Parameters({"ADJOURNED_NOTIFICATION", "APPEAL_LAPSED_NOTIFICATION"})
+    public void willThrowExceptionIfTriesToHandleEvents(NotificationEventType notificationEventType) {
+        SscsCaseDataWrapper callback = SscsCaseDataWrapper.builder().notificationEventType(notificationEventType).build();
+        handler.handle(callback);
+    }
+
+}
---
+++ src/test/java/uk/gov/hmcts/reform/sscs/callback/handlers/HandlerHelper.java
@@ -0,0 +1,22 @@
+package uk.gov.hmcts.reform.sscs.callback.handlers;
+
+import uk.gov.hmcts.reform.sscs.ccd.domain.EventType;
+import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
+import uk.gov.hmcts.reform.sscs.ccd.domain.State;
+import uk.gov.hmcts.reform.sscs.domain.SscsCaseDataWrapper;
+import uk.gov.hmcts.reform.sscs.domain.notify.NotificationEventType;
+
+public class HandlerHelper {
+
+    private HandlerHelper() {
+
+    }
+
+    public static SscsCaseDataWrapper buildTestCallbackForGivenData(SscsCaseData sscsCaseData, State state, NotificationEventType eventType) {
+        return SscsCaseDataWrapper.builder()
+                .newSscsCaseData(sscsCaseData)
+                .oldSscsCaseData(sscsCaseData)
+                .notificationEventType(eventType)
+                .state(state).build();
+    }
+}
