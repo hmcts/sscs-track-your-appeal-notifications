7a65171bd1c22058e37af70c9ea8fd3902dc3735
Ryan Da Costa <369407+ryandac@users.noreply.github.com>
2019-10-02 11:29:51 +0100

    SSCS-5210 - add notifications subscription (#653)
    * SSCS-5210 AddNotificationsSubscription

---
+++ src/test/java/uk/gov/hmcts/reform/sscs/servicebus/TopicConsumerTest.java
@@ -0,0 +1,81 @@
+package uk.gov.hmcts.reform.sscs.servicebus;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+import java.util.Optional;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import uk.gov.hmcts.reform.sscs.callback.CallbackDispatcher;
+import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
+import uk.gov.hmcts.reform.sscs.ccd.deserialisation.SscsCaseCallbackDeserializer;
+import uk.gov.hmcts.reform.sscs.ccd.domain.CaseDetails;
+import uk.gov.hmcts.reform.sscs.ccd.domain.EventType;
+import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
+import uk.gov.hmcts.reform.sscs.ccd.domain.State;
+import uk.gov.hmcts.reform.sscs.exception.ClientAuthorisationException;
+
+public class TopicConsumerTest {
+
+    private static final String MESSAGE = "message";
+    private static final Exception EXCEPTION = new RuntimeException("blah");
+
+    @Mock
+    private CallbackDispatcher dispatcher;
+
+    @Mock
+    private SscsCaseCallbackDeserializer deserializer;
+
+    private TopicConsumer topicConsumer;
+    private Exception exception;
+
+    @Before
+    public void setup() {
+        initMocks(this);
+        topicConsumer = new TopicConsumer(dispatcher, deserializer);
+        CaseDetails<SscsCaseData> caseDetails = new CaseDetails<>(
+                123L,
+                "jurisdiction",
+                State.APPEAL_CREATED,
+                SscsCaseData.builder().build(),
+                null
+        );
+        Callback<SscsCaseData> callback = new Callback<>(caseDetails, Optional.empty(), EventType.EVIDENCE_RECEIVED);
+        when(deserializer.deserialize(any())).thenReturn(callback);
+    }
+
+    @Test
+    public void anExceptionWillBeCaught() {
+        exception = EXCEPTION;
+        doThrow(exception).when(dispatcher).handle(any());
+        topicConsumer.onMessage(MESSAGE);
+        verify(dispatcher, atLeastOnce()).handle(any());
+    }
+
+
+    @Test
+    public void nullPointerExceptionWillBeCaught() {
+        exception = new NullPointerException();
+        doThrow(exception).when(dispatcher).handle(any());
+        topicConsumer.onMessage(MESSAGE);
+        verify(dispatcher, atLeastOnce()).handle(any());
+    }
+
+    @Test
+    public void clientAuthorisationExceptionWillBeCaught() {
+        exception = new ClientAuthorisationException(EXCEPTION);
+        doThrow(exception).when(dispatcher).handle(any());
+        topicConsumer.onMessage(MESSAGE);
+        verify(dispatcher, atLeastOnce()).handle(any());
+    }
+
+    @Test
+    public void handleValidRequest() {
+        topicConsumer.onMessage(MESSAGE);
+        verify(dispatcher).handle(any());
+    }
+
+}
