c813e81824441a124b6d8e67779e77c55128c703
david.crespo <david.crespo@hmcts.net>
2019-10-15 01:28:25 +0200

    add more tests for other methods

--- src/test/java/uk/gov/hmcts/reform/sscs/callback/handlers/AdminAppealWithdrawnNotificationsEventsHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/callback/handlers/AdminAppealWithdrawnNotificationsEventsHandlerTest.java
@@ -1,17 +1,23 @@
 package uk.gov.hmcts.reform.sscs.callback.handlers;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static uk.gov.hmcts.reform.sscs.domain.notify.NotificationEventType.ADJOURNED_NOTIFICATION;
+import static uk.gov.hmcts.reform.sscs.domain.notify.NotificationEventType.ADMIN_APPEAL_WITHDRAWN;
 
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 import uk.gov.hmcts.reform.sscs.domain.SscsCaseDataWrapper;
 import uk.gov.hmcts.reform.sscs.domain.notify.NotificationEventType;
+import uk.gov.hmcts.reform.sscs.factory.CcdNotificationWrapper;
 import uk.gov.hmcts.reform.sscs.service.NotificationService;
 
 @RunWith(JUnitParamsRunner.class)
@@ -22,8 +28,9 @@ public class AdminAppealWithdrawnNotificationsEventsHandlerTest {
 
     @Mock
     private NotificationService notificationService;
-    private AdminAppealWithdrawnNotificationsEventsHandler handler =
-        new AdminAppealWithdrawnNotificationsEventsHandler(notificationService);
+
+    @InjectMocks
+    private AdminAppealWithdrawnNotificationsEventsHandler handler;
 
     @Test
     @Parameters({"ADMIN_APPEAL_WITHDRAWN, true", "ADJOURNED_NOTIFICATION, false", "SYA_APPEAL_CREATED_NOTIFICATION, false"})
@@ -31,14 +38,28 @@ public class AdminAppealWithdrawnNotificationsEventsHandlerTest {
         SscsCaseDataWrapper callback = SscsCaseDataWrapper.builder()
             .notificationEventType(notificationEventType)
             .build();
-
-        boolean currentResult = handler.canHandle(callback);
-
-        assertEquals(expectedResult, currentResult);
+        assertEquals(expectedResult, handler.canHandle(callback));
     }
 
     @Test
     public void handle() {
+        SscsCaseDataWrapper callback = SscsCaseDataWrapper.builder()
+            .notificationEventType(ADMIN_APPEAL_WITHDRAWN)
+            .build();
+        Mockito.doNothing().when(notificationService).manageNotificationAndSubscription(any(CcdNotificationWrapper.class));
+
+        handler.handle(callback);
+
+        Mockito.verify(notificationService, Mockito.times(1))
+            .manageNotificationAndSubscription(any(CcdNotificationWrapper.class));
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void handleThrowException() {
+        SscsCaseDataWrapper callback = SscsCaseDataWrapper.builder()
+            .notificationEventType(ADJOURNED_NOTIFICATION)
+            .build();
+        handler.handle(callback);
     }
 
     @Test
