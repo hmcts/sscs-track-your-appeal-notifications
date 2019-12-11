c39ac80ea87521c09b63886b20f25cf87f288eb7
srochani <sunil.rochani@hotmail.com>
2019-12-11 16:12:09 +0000

    SSCS-6971 Send docmosis appeal lodged letter with coversheet, instead of notify template when RTL goes live

--- src/test/java/uk/gov/hmcts/reform/sscs/service/SendNotificationServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/service/SendNotificationServiceTest.java
@@ -2,12 +2,10 @@ package uk.gov.hmcts.reform.sscs.service;
 
 import static org.junit.Assert.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 import static org.mockito.MockitoAnnotations.initMocks;
 import static uk.gov.hmcts.reform.sscs.ccd.domain.State.READY_TO_LIST;
 import static uk.gov.hmcts.reform.sscs.ccd.domain.State.VALID_APPEAL;
-import static uk.gov.hmcts.reform.sscs.config.AppConstants.ADDRESS_LINE_1;
 import static uk.gov.hmcts.reform.sscs.config.AppConstants.REP_SALUTATION;
 import static uk.gov.hmcts.reform.sscs.config.SubscriptionType.*;
 import static uk.gov.hmcts.reform.sscs.domain.notify.NotificationEventType.*;
