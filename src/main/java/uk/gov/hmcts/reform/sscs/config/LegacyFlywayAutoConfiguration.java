7d9bedfe4de7a25c64884de41f646022071d9947
srochani <sunil.rochani@hotmail.com>
2020-04-14 23:07:35 +0100

    SSCS-7333: Notifications – Upgrading the reminder service DB to PostgreSQL 11 SSCS-7414: Tech debt - Upgrade Flyway database

--- src/main/java/uk/gov/hmcts/reform/sscs/config/LegacyFlywayAutoConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/config/LegacyFlywayAutoConfiguration.java
@@ -33,9 +33,7 @@ public class LegacyFlywayAutoConfiguration {
 
     @Bean(initMethod = "migrate")
     public Flyway flyway(DataSource dataSource) {
-        Flyway flyway = new Flyway();
-        flyway.setBaselineOnMigrate(true);
-        flyway.setDataSource(dataSource);
+        Flyway flyway = Flyway.configure().baselineOnMigrate(true).dataSource(dataSource).load();
         return flyway;
     }
 
