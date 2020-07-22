4caf0ddcaee6e4750714c91eb87512c561119cd7
Ryan Da Costa <ryan.dacosta1@hmcts.net>
2020-07-22 13:39:42 +0100

    SSCS-7491 - Notification – Moving all notifications over to a queue

--- src/main/java/uk/gov/hmcts/reform/sscs/callback/handlers/FilterNotificationsEventsHandler.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/callback/handlers/FilterNotificationsEventsHandler.java
@@ -1,5 +1,31 @@
 package uk.gov.hmcts.reform.sscs.callback.handlers;
 
+import static java.util.Arrays.asList;
+import static java.util.Collections.unmodifiableList;
+import static uk.gov.hmcts.reform.sscs.domain.notify.NotificationEventType.ADJOURNED_NOTIFICATION;
+import static uk.gov.hmcts.reform.sscs.domain.notify.NotificationEventType.ADMIN_APPEAL_WITHDRAWN;
+import static uk.gov.hmcts.reform.sscs.domain.notify.NotificationEventType.APPEAL_DORMANT_NOTIFICATION;
+import static uk.gov.hmcts.reform.sscs.domain.notify.NotificationEventType.APPEAL_LAPSED_NOTIFICATION;
+import static uk.gov.hmcts.reform.sscs.domain.notify.NotificationEventType.APPEAL_RECEIVED_NOTIFICATION;
+import static uk.gov.hmcts.reform.sscs.domain.notify.NotificationEventType.APPEAL_WITHDRAWN_NOTIFICATION;
+import static uk.gov.hmcts.reform.sscs.domain.notify.NotificationEventType.DECISION_ISSUED;
+import static uk.gov.hmcts.reform.sscs.domain.notify.NotificationEventType.DIRECTION_ISSUED;
+import static uk.gov.hmcts.reform.sscs.domain.notify.NotificationEventType.DWP_APPEAL_LAPSED_NOTIFICATION;
+import static uk.gov.hmcts.reform.sscs.domain.notify.NotificationEventType.DWP_RESPONSE_RECEIVED_NOTIFICATION;
+import static uk.gov.hmcts.reform.sscs.domain.notify.NotificationEventType.DWP_UPLOAD_RESPONSE_NOTIFICATION;
+import static uk.gov.hmcts.reform.sscs.domain.notify.NotificationEventType.EVIDENCE_RECEIVED_NOTIFICATION;
+import static uk.gov.hmcts.reform.sscs.domain.notify.NotificationEventType.HEARING_BOOKED_NOTIFICATION;
+import static uk.gov.hmcts.reform.sscs.domain.notify.NotificationEventType.ISSUE_FINAL_DECISION;
+import static uk.gov.hmcts.reform.sscs.domain.notify.NotificationEventType.NON_COMPLIANT_NOTIFICATION;
+import static uk.gov.hmcts.reform.sscs.domain.notify.NotificationEventType.POSTPONEMENT_NOTIFICATION;
+import static uk.gov.hmcts.reform.sscs.domain.notify.NotificationEventType.REISSUE_DOCUMENT;
+import static uk.gov.hmcts.reform.sscs.domain.notify.NotificationEventType.REQUEST_INFO_INCOMPLETE;
+import static uk.gov.hmcts.reform.sscs.domain.notify.NotificationEventType.RESEND_APPEAL_CREATED_NOTIFICATION;
+import static uk.gov.hmcts.reform.sscs.domain.notify.NotificationEventType.STRUCK_OUT;
+import static uk.gov.hmcts.reform.sscs.domain.notify.NotificationEventType.SUBSCRIPTION_UPDATED_NOTIFICATION;
+import static uk.gov.hmcts.reform.sscs.domain.notify.NotificationEventType.VALID_APPEAL_CREATED;
+
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.sscs.callback.CallbackHandler;
@@ -12,6 +38,30 @@ import uk.gov.hmcts.reform.sscs.service.NotificationService;
 @Service
 public class FilterNotificationsEventsHandler implements CallbackHandler {
 
+    private final List<NotificationEventType> EVENTS_LIST = unmodifiableList(asList(
+            ADJOURNED_NOTIFICATION,
+            ADMIN_APPEAL_WITHDRAWN,
+            APPEAL_DORMANT_NOTIFICATION,
+            APPEAL_LAPSED_NOTIFICATION,
+            APPEAL_RECEIVED_NOTIFICATION,
+            APPEAL_WITHDRAWN_NOTIFICATION,
+            DECISION_ISSUED,
+            DIRECTION_ISSUED,
+            DWP_APPEAL_LAPSED_NOTIFICATION,
+            DWP_RESPONSE_RECEIVED_NOTIFICATION,
+            DWP_UPLOAD_RESPONSE_NOTIFICATION,
+            EVIDENCE_RECEIVED_NOTIFICATION,
+            HEARING_BOOKED_NOTIFICATION,
+            ISSUE_FINAL_DECISION,
+            NON_COMPLIANT_NOTIFICATION,
+            POSTPONEMENT_NOTIFICATION,
+            REISSUE_DOCUMENT,
+            REQUEST_INFO_INCOMPLETE,
+            RESEND_APPEAL_CREATED_NOTIFICATION,
+            STRUCK_OUT,
+            SUBSCRIPTION_UPDATED_NOTIFICATION,
+            VALID_APPEAL_CREATED
+    ));
     private final NotificationService notificationService;
 
     @Autowired
@@ -21,12 +71,7 @@ public class FilterNotificationsEventsHandler implements CallbackHandler {
 
     @Override
     public boolean canHandle(SscsCaseDataWrapper callback) {
-        return callback.getNotificationEventType() == NotificationEventType.DWP_UPLOAD_RESPONSE_NOTIFICATION
-                || callback.getNotificationEventType() == NotificationEventType.DIRECTION_ISSUED
-                || callback.getNotificationEventType() == NotificationEventType.VALID_APPEAL_CREATED
-                || callback.getNotificationEventType() == NotificationEventType.STRUCK_OUT
-                || callback.getNotificationEventType() == NotificationEventType.NON_COMPLIANT_NOTIFICATION
-                || callback.getNotificationEventType() == NotificationEventType.REISSUE_DOCUMENT;
+        return EVENTS_LIST.contains(callback.getNotificationEventType());
     }
 
     @Override
