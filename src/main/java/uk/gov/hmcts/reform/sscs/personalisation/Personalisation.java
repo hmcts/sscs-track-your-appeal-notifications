41edcc32bfe394067b5f98bc725efa7251cc541c
Ryan Da Costa <369407+ryandac@users.noreply.github.com>
2020-09-08 14:26:31 +0100

    SSCS-7808 - Appeal to proceed (#1136)
    * SSCS-7808 - Appeal to proceed

--- src/main/java/uk/gov/hmcts/reform/sscs/personalisation/Personalisation.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/personalisation/Personalisation.java
@@ -452,7 +452,7 @@ public class Personalisation<E extends NotificationWrapper> {
         String smsTemplateName = isSendSmsSubscriptionConfirmation() ? SUBSCRIPTION_CREATED_NOTIFICATION.getId() + "." + subscriptionType.toString().toLowerCase() :
             templateConfig;
         String letterTemplateName = getLetterTemplateName(subscriptionType, notificationWrapper.getNotificationType());
-        String docmosisTemplateName = getDocmosisTemplateName(subscriptionType, notificationWrapper.getNotificationType());
+        String docmosisTemplateName = getDocmosisTemplateName(subscriptionType, notificationWrapper.getNotificationType(), notificationWrapper.getNewSscsCaseData());
 
         return config.getTemplate(templateConfig, smsTemplateName, letterTemplateName, docmosisTemplateName,
             benefit, notificationWrapper.getHearingType(), notificationWrapper.getNewSscsCaseData().getCreatedInGapsFrom(),
@@ -491,9 +491,12 @@ public class Personalisation<E extends NotificationWrapper> {
         return emailTemplateName;
     }
 
-    private String getDocmosisTemplateName(SubscriptionType subscriptionType, NotificationEventType notificationEventType) {
+    private String getDocmosisTemplateName(SubscriptionType subscriptionType, NotificationEventType notificationEventType, SscsCaseData caseData) {
         String letterTemplateName = notificationEventType.getId();
-        if (subscriptionType != null
+        if (subscriptionType != null && DIRECTION_ISSUED.equals(notificationEventType)
+                && caseData.getDirectionTypeDl() != null && caseData.getDirectionTypeDl().getValue() != null) {
+            letterTemplateName = letterTemplateName + "." + caseData.getDirectionTypeDl().getValue().getCode() + "." + subscriptionType.name().toLowerCase();
+        } else if (subscriptionType != null
             && (APPEAL_RECEIVED_NOTIFICATION.equals(notificationEventType)
             || DIRECTION_ISSUED.equals(notificationEventType)
             || DECISION_ISSUED.equals(notificationEventType)
@@ -502,6 +505,7 @@ public class Personalisation<E extends NotificationWrapper> {
             || ISSUE_ADJOURNMENT_NOTICE.equals(notificationEventType))) {
             letterTemplateName = letterTemplateName + "." + subscriptionType.name().toLowerCase();
         }
+
         return letterTemplateName;
     }
 
