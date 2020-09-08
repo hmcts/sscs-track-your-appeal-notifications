07127b0dced190970599f66fb21aef86d8789f26
ruban72 <ruban72@hotmail.com>
2020-09-08 14:51:47 +0100

    WLA-595: Merge from master

--- src/main/java/uk/gov/hmcts/reform/sscs/personalisation/Personalisation.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/personalisation/Personalisation.java
@@ -587,14 +587,14 @@ public class Personalisation<E extends NotificationWrapper> {
                 && caseData.getDirectionTypeDl() != null && caseData.getDirectionTypeDl().getValue() != null) {
             letterTemplateName = letterTemplateName + "." + caseData.getDirectionTypeDl().getValue().getCode() + "." + subscriptionType.name().toLowerCase();
         } else if (subscriptionType != null
-            && (APPEAL_RECEIVED_NOTIFICATION.equals(notificationEventType)
-            || DIRECTION_ISSUED.equals(notificationEventType)
-            || DECISION_ISSUED.equals(notificationEventType)
+                && (APPEAL_RECEIVED_NOTIFICATION.equals(notificationEventType)
+                || DIRECTION_ISSUED.equals(notificationEventType)
+                || DECISION_ISSUED.equals(notificationEventType)
                 || DIRECTION_ISSUED_WELSH.equals(notificationEventType)
                 || DECISION_ISSUED_WELSH.equals(notificationEventType)
-            || REQUEST_INFO_INCOMPLETE.equals(notificationEventType)
-            || ISSUE_FINAL_DECISION.equals(notificationEventType)
-            || ISSUE_ADJOURNMENT_NOTICE.equals(notificationEventType))) {
+                || REQUEST_INFO_INCOMPLETE.equals(notificationEventType)
+                || ISSUE_FINAL_DECISION.equals(notificationEventType)
+                || ISSUE_ADJOURNMENT_NOTICE.equals(notificationEventType))) {
             letterTemplateName = letterTemplateName + "." + subscriptionType.name().toLowerCase();
         }
 
