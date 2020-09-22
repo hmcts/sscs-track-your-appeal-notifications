d14c4a7ba6805ae120efa147530407ddf9307403
Ryan Da Costa <ryan.dacosta1@hmcts.net>
2020-09-22 13:37:08 +0100

    SSCS-7976 - Refuse Extension for Joint party

--- src/IntegrationTests/java/uk/gov/hmcts/reform/sscs/config/NotificationConfigTest.java
+++ src/IntegrationTests/java/uk/gov/hmcts/reform/sscs/config/NotificationConfigTest.java
@@ -121,7 +121,7 @@ public class NotificationConfigTest {
             "APPEAL_TO_PROCEED, TB-SCS-GNO-ENG-00551.docx, TB-SCS-GNO-ENG-00551.docx, TB-SCS-GNO-ENG-00551.docx",
             "PROVIDE_INFORMATION, TB-SCS-GNO-ENG-00067.docx, TB-SCS-GNO-ENG-00089.docx, null",
             "GRANT_EXTENSION, TB-SCS-GNO-ENG-00556.docx, TB-SCS-GNO-ENG-00556.docx, null",
-            "REFUSE_EXTENSION, TB-SCS-GNO-ENG-00557.docx, TB-SCS-GNO-ENG-00557.docx, null",
+            "REFUSE_EXTENSION, TB-SCS-GNO-ENG-00557.docx, TB-SCS-GNO-ENG-00557.docx, TB-SCS-GNO-ENG-00557.docx",
             "APPEAL_TO_PROCEED, TB-SCS-GNO-WEL-00468.docx, TB-SCS-GNO-WEL-00472.docx, null",
             "PROVIDE_INFORMATION, TB-SCS-GNO-WEL-00468.docx, TB-SCS-GNO-WEL-00472.docx, null",
             "GRANT_EXTENSION, TB-SCS-GNO-WEL-00468.docx, TB-SCS-GNO-WEL-00472.docx, null",
