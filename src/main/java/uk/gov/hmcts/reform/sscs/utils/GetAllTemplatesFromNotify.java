a06bb7f3bbe9436edadd33c3769879b60f702e3b
Chris Davidson <christopher.davidson@btinternet.com>
2021-04-12 15:23:46 +0100

    sscs 9045 class to pull templates

---
+++ src/main/java/uk/gov/hmcts/reform/sscs/utils/GetAllTemplatesFromNotify.java
@@ -0,0 +1,67 @@
+package uk.gov.hmcts.reform.sscs.utils;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import uk.gov.service.notify.NotificationClient;
+import uk.gov.service.notify.NotificationClientException;
+import uk.gov.service.notify.Template;
+import uk.gov.service.notify.TemplateList;
+
+public final class GetAllTemplatesFromNotify {
+
+    public static NotificationClient testNotificationClient() {
+        return new NotificationClient(testApiKey);
+    }
+
+    private GetAllTemplatesFromNotify() {
+
+    }
+
+    private static final String testApiKey = "enter test key here";
+
+    public static void main(String[] args) {
+        String var = "benefit_name_acronym";
+        NotificationClient client = testNotificationClient();
+        try {
+            //empty is everything, can be email sms or letter
+            String type = "";
+            TemplateList templates = client.getAllTemplates(type);
+
+            System.out.println("Found " + templates.getTemplates().size() + " templates in total");
+
+            int count = 0;
+
+            for (Template template: templates.getTemplates()) {
+                if (template.getPersonalisation().get().keySet().contains(var)) {
+                    if (template.getName().contains("COR") || template.getName().contains("NOT USED")
+                    || template.getName().contains("OLD")) {
+                        System.out.println("Excluding: " + template.getName());
+                        continue;
+                    }
+                    System.out.println("Name: " + template.getName());
+                    System.out.println("ID: " + template.getId());
+                    System.out.println("Type: " + template.getTemplateType());
+                    if (template.getSubject().isPresent()) {
+                        System.out.println("Subject for " + template.getSubject().get());
+                    }
+                    System.out.println("Body is: " + template.getBody());
+                    count++;
+                    continue;
+                } else {
+                    System.out.println("This template does not contain " + var);
+                    System.out.println("Name: " + template.getName());
+                    System.out.println("ID: " + template.getId());
+                    System.out.println("Type: " + template.getTemplateType());
+                    System.out.println("Body is: " + template.getBody());
+                }
+
+                System.out.println("************************************************************");
+            }
+            System.out.println("There were " + count + " templates found with key " + var);
+        } catch (NotificationClientException e) {
+            e.printStackTrace(System.out);
+        }
+    }
+}
