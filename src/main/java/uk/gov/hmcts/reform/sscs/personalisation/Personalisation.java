e755c4d0771d163287a0d2dd7ca3e5d552c3df5d
Jason.Hart <jason.hart@enginegroup.com>
2021-04-15 14:35:43 +0100

    replace acronym with full name if acronym doesnt exist

--- src/main/java/uk/gov/hmcts/reform/sscs/personalisation/Personalisation.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/personalisation/Personalisation.java
@@ -175,7 +175,11 @@ public class Personalisation<E extends NotificationWrapper> {
                 && ccdResponse.getAppeal().getBenefitType() != null
                 && !isEmpty(ccdResponse.getAppeal().getBenefitType().getCode())) {
                 benefit = getBenefitByCode(ccdResponse.getAppeal().getBenefitType().getCode());
-                personalisation.put(BENEFIT_NAME_ACRONYM_LITERAL, benefit.name());
+                if (benefit.isHasAcronym()) {
+                    personalisation.put(BENEFIT_NAME_ACRONYM_LITERAL, benefit.name());
+                } else {
+                    personalisation.put(BENEFIT_NAME_ACRONYM_LITERAL, benefit.getDescription());
+                }
                 personalisation.put(BENEFIT_NAME_ACRONYM_SHORT_LITERAL, benefit.name());
                 personalisation.put(BENEFIT_FULL_NAME_LITERAL, benefit.getDescription());
             } else {
