474ab09bdd32efc5ba35e39fbe1ed71d79b624d1
Jason.Hart <jason.hart@enginegroup.com>
2021-10-04 16:06:06 +0100

    SSCS-9591-upgrade-dependencies

--- src/main/java/uk/gov/hmcts/reform/sscs/config/LegacyFlywayAutoConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/config/LegacyFlywayAutoConfiguration.java
@@ -3,17 +3,13 @@ package uk.gov.hmcts.reform.sscs.config;
 import javax.sql.DataSource;
 import org.flywaydb.core.Flyway;
 import org.springframework.beans.factory.ObjectProvider;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationInitializer;
-import org.springframework.boot.autoconfigure.jdbc.JdbcOperationsDependsOnPostProcessor;
 import org.springframework.boot.jdbc.SchemaManagement;
 import org.springframework.boot.jdbc.SchemaManagementProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.jdbc.core.JdbcOperations;
 
 @Configuration
 @ConditionalOnProperty(prefix = "spring.flyway", name = "enabled", matchIfMissing = true)
@@ -47,7 +43,7 @@ public class LegacyFlywayAutoConfiguration {
      * Additional configuration to ensure that {@link JdbcOperations} beans depend
      * on the {@code flywayInitializer} bean.
      */
-    @Configuration
+    /*@Configuration
     @ConditionalOnClass(JdbcOperations.class)
     @ConditionalOnBean(JdbcOperations.class)
     protected static class FlywayInitializerJdbcOperationsDependencyConfiguration
@@ -57,5 +53,5 @@ public class LegacyFlywayAutoConfiguration {
             super("flywayInitializer");
         }
 
-    }
+    }*/
 }
