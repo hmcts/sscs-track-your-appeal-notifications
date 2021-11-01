9438e681cf6f37a0d137fb242c55c5375b23ea44
Chris Davidson <christopher.davidson@btinternet.com>
2021-11-01 16:17:46 +0000

    sscs 8709 checkstyle

--- src/e2e/java/uk/gov/hmcts/reform/sscs/functional/sya/notifications/NotificationsFunctionalTest.java
+++ src/e2e/java/uk/gov/hmcts/reform/sscs/functional/sya/notifications/NotificationsFunctionalTest.java
@@ -5,7 +5,6 @@ import static uk.gov.hmcts.reform.sscs.domain.notify.NotificationEventType.*;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 import junitparams.Parameters;
 import org.junit.Ignore;
