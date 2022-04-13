498d84c7101b36b1be5c4a7a619b6cf460ce63ad
Mohammed Qureshi <mohammed.qureshi@HMCTS.NET>
2022-04-13 16:12:19 +0100

    SSCS-10376 Update Docket

--- src/main/java/uk/gov/hmcts/reform/sscs/config/SwaggerConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/config/SwaggerConfiguration.java
@@ -3,20 +3,9 @@ package uk.gov.hmcts.reform.sscs.config;
 import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import uk.gov.hmcts.reform.sscs.TrackYourAppealNotificationsApplication;
 
 @Configuration
 public class SwaggerConfiguration {
-
-//    @Bean
-//    public Docket api() {
-//        return new Docket(DocumentationType.SWAGGER_2)
-//            .useDefaultResponseMessages(false)
-//            .select()
-//            .apis(RequestHandlerSelectors.basePackage(TrackYourAppealNotificationsApplication.class.getPackage().getName() + ".controllers"))
-//            .paths(PathSelectors.any())
-//            .build();
-//    }
 
     @Bean
     public GroupedOpenApi api() {
