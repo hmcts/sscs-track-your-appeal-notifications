06b83b47b1b88ff55f149bfc0b5f262fba2b5d06
Mohammed Qureshi <mohammed.qureshi@HMCTS.NET>
2022-04-13 16:12:04 +0100

    SSCS-10376 Update Docket

--- src/main/java/uk/gov/hmcts/reform/sscs/config/SwaggerConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/config/SwaggerConfiguration.java
@@ -1,25 +1,27 @@
 package uk.gov.hmcts.reform.sscs.config;
 
+import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import uk.gov.hmcts.reform.sscs.TrackYourAppealNotificationsApplication;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfiguration {
 
+//    @Bean
+//    public Docket api() {
+//        return new Docket(DocumentationType.SWAGGER_2)
+//            .useDefaultResponseMessages(false)
+//            .select()
+//            .apis(RequestHandlerSelectors.basePackage(TrackYourAppealNotificationsApplication.class.getPackage().getName() + ".controllers"))
+//            .paths(PathSelectors.any())
+//            .build();
+//    }
+
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-            .useDefaultResponseMessages(false)
-            .select()
-            .apis(RequestHandlerSelectors.basePackage(TrackYourAppealNotificationsApplication.class.getPackage().getName() + ".controllers"))
-            .paths(PathSelectors.any())
+    public GroupedOpenApi api() {
+        return GroupedOpenApi.builder()
+            .pathsToMatch("*")
             .build();
     }
 
