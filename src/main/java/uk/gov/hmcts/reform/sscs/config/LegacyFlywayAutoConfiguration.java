04483c5a5e8ce566aed3321bbb1219f034fbbc72
Nick Hill <nicholas.hill@version1.com>
2022-11-03 12:24:37 +0000

    updated flyway init SSCS-11053

--- src/main/java/uk/gov/hmcts/reform/sscs/config/LegacyFlywayAutoConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/config/LegacyFlywayAutoConfiguration.java
@@ -1,16 +1,14 @@
 package uk.gov.hmcts.reform.sscs.config;
 
-import java.util.Set;
 import javax.sql.DataSource;
 import org.flywaydb.core.Flyway;
-import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationInitializer;
+import org.springframework.boot.autoconfigure.orm.jpa.EntityManagerFactoryDependsOnPostProcessor;
 import org.springframework.boot.jdbc.SchemaManagement;
 import org.springframework.boot.jdbc.SchemaManagementProvider;
-import org.springframework.boot.sql.init.dependency.DependsOnDatabaseInitializationDetector;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -46,15 +44,10 @@ public class LegacyFlywayAutoConfiguration {
     @ConditionalOnClass(JdbcOperations.class)
     @ConditionalOnBean(JdbcOperations.class)
     protected static class FlywayInitializerDependsOnDatabaseInitializationDetector
-        implements DependsOnDatabaseInitializationDetector {
+        extends EntityManagerFactoryDependsOnPostProcessor {
 
         public FlywayInitializerDependsOnDatabaseInitializationDetector() {
-            super();
-        }
-
-        @Override
-        public Set<String> detect(ConfigurableListableBeanFactory beanFactory) {
-            return null;
+            super("flywayInitializer");
         }
     }
 }
