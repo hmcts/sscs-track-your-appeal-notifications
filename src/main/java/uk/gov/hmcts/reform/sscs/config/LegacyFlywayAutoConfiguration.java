59b9de0eeea46dbce448ba06f4201632f340e84a
Nick Hill <nicholas.hill@version1.com>
2022-11-04 14:47:47 +0000

    remove unused imports SSCS-11053

--- src/main/java/uk/gov/hmcts/reform/sscs/config/LegacyFlywayAutoConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/config/LegacyFlywayAutoConfiguration.java
@@ -1,22 +1,15 @@
 package uk.gov.hmcts.reform.sscs.config;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 import javax.sql.DataSource;
 import org.flywaydb.core.Flyway;
 import org.springframework.beans.factory.ObjectProvider;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationInitializer;
 import org.springframework.boot.jdbc.SchemaManagement;
 import org.springframework.boot.jdbc.SchemaManagementProvider;
-import org.springframework.boot.sql.init.dependency.AbstractBeansOfTypeDependsOnDatabaseInitializationDetector;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.jdbc.core.JdbcOperations;
 
 @Configuration
 @ConditionalOnProperty(prefix = "spring.flyway", name = "enabled", matchIfMissing = true)
