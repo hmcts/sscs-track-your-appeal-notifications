675b4ca6fb2652e9bbf0df85aca2e20dcd12e516
Nick Hill <nicholas.hill@version1.com>
2022-11-04 16:37:17 +0000

    add back FlywayInitializerJdbcOperationsDependencyConfiguration SSCS-11053

--- src/main/java/uk/gov/hmcts/reform/sscs/config/LegacyFlywayAutoConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/config/LegacyFlywayAutoConfiguration.java
@@ -1,15 +1,22 @@
 package uk.gov.hmcts.reform.sscs.config;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import javax.sql.DataSource;
 import org.flywaydb.core.Flyway;
 import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationInitializer;
 import org.springframework.boot.jdbc.SchemaManagement;
 import org.springframework.boot.jdbc.SchemaManagementProvider;
+import org.springframework.boot.sql.init.dependency.AbstractBeansOfTypeDependsOnDatabaseInitializationDetector;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.core.JdbcOperations;
 
 @Configuration
 @ConditionalOnProperty(prefix = "spring.flyway", name = "enabled", matchIfMissing = true)
@@ -37,5 +44,21 @@ public class LegacyFlywayAutoConfiguration {
     @Bean
     public FlywayMigrationInitializer flywayInitializer(Flyway flyway) {
         return new FlywayMigrationInitializer(flyway, null);
+    }
+
+    @Configuration
+    @ConditionalOnClass(JdbcOperations.class)
+    @ConditionalOnBean(JdbcOperations.class)
+    protected static class FlywayInitializerJdbcOperationsDependencyConfiguration
+        extends AbstractBeansOfTypeDependsOnDatabaseInitializationDetector {
+
+        public FlywayInitializerJdbcOperationsDependencyConfiguration() {
+            super();
+        }
+
+        @Override
+        protected Set<Class<?>> getDependsOnDatabaseInitializationBeanTypes() {
+            return new HashSet<>(List.of(JdbcOperations.class));
+        }
     }
 }
