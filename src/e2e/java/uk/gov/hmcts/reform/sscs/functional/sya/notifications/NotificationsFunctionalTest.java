9b8fb8a9c546a132fd04950091cfe10007781966
eliandro goncalves <eliandro.goncalves@version1.com>
2022-12-16 14:54:06 +0000

    test fix

--- src/e2e/java/uk/gov/hmcts/reform/sscs/functional/sya/notifications/NotificationsFunctionalTest.java
+++ src/e2e/java/uk/gov/hmcts/reform/sscs/functional/sya/notifications/NotificationsFunctionalTest.java
@@ -404,7 +404,7 @@ public class NotificationsFunctionalTest extends AbstractFunctionalTest {
         );
         assertThat(notifications)
             .hasSize(2)
-            .anySatisfy((notification)->{
+            .anySatisfy((notification) -> {
                 assertThat(notification.getId().toString()).isEqualTo(paperAppointeeResponseReceivedEmailId);
                 assertThat(notification.getBody())
                     .contains("Dear Appointee User")
