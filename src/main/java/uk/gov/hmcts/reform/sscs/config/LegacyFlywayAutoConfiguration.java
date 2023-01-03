5132a77d019c955f9b95060ec1fe3b258ff96e30
nickhill111 <nickhill11@live.co.uk>
2023-01-03 11:25:05 +0000

    update legacyflywayautoconfig SSCS-11053

--- src/main/java/uk/gov/hmcts/reform/sscs/config/LegacyFlywayAutoConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/config/LegacyFlywayAutoConfiguration.java
@@ -1,5 +1,8 @@
 package uk.gov.hmcts.reform.sscs.config;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import javax.sql.DataSource;
 import org.flywaydb.core.Flyway;
 import org.springframework.beans.factory.ObjectProvider;
@@ -7,9 +10,9 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationInitializer;
-import org.springframework.boot.autoconfigure.jdbc.JdbcOperationsDependsOnPostProcessor;
 import org.springframework.boot.jdbc.SchemaManagement;
 import org.springframework.boot.jdbc.SchemaManagementProvider;
+import org.springframework.boot.sql.init.dependency.AbstractBeansOfTypeDependsOnDatabaseInitializationDetector;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -22,13 +25,7 @@ public class LegacyFlywayAutoConfiguration {
     @Bean
     @Primary
     public SchemaManagementProvider flywayDefaultDdlModeProvider(ObjectProvider<Flyway> flyways) {
-        return new SchemaManagementProvider() {
-
-            @Override
-            public SchemaManagement getSchemaManagement(DataSource dataSource) {
-                return SchemaManagement.MANAGED;
-            }
-        };
+        return dataSource -> SchemaManagement.MANAGED;
     }
 
     @Bean(initMethod = "migrate")
@@ -51,11 +48,15 @@ public class LegacyFlywayAutoConfiguration {
     @ConditionalOnClass(JdbcOperations.class)
     @ConditionalOnBean(JdbcOperations.class)
     protected static class FlywayInitializerJdbcOperationsDependencyConfiguration
-            extends JdbcOperationsDependsOnPostProcessor {
+        extends AbstractBeansOfTypeDependsOnDatabaseInitializationDetector {
 
         public FlywayInitializerJdbcOperationsDependencyConfiguration() {
-            super("flywayInitializer");
+            super();
         }
 
+        @Override
+        protected Set<Class<?>> getDependsOnDatabaseInitializationBeanTypes() {
+            return new HashSet<>(List.of(JdbcOperations.class));
+        }
     }
 }
