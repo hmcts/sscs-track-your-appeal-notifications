dc6e052cedc8e58689062fc3eceb6f29d2542c92
nickhill111 <nickhill11@live.co.uk>
2023-01-05 19:39:33 +0000

    fix failing test with updated template SSCS-common-4.x

--- src/e2e/java/uk/gov/hmcts/reform/sscs/functional/sya/notifications/NotificationsFunctionalTest.java
+++ src/e2e/java/uk/gov/hmcts/reform/sscs/functional/sya/notifications/NotificationsFunctionalTest.java
@@ -400,7 +400,7 @@ public class NotificationsFunctionalTest extends AbstractFunctionalTest {
         );
         Notification emailNotification = notifications.stream().filter(f -> f.getTemplateId().toString().equals(paperAppointeeResponseReceivedEmailId)).collect(Collectors.toList()).get(0);
         assertTrue(emailNotification.getBody().contains("Dear Appointee User"));
-        assertTrue(emailNotification.getBody().contains("You should have received a copy"));
+        assertTrue(emailNotification.getBody().contains("They should have sent you a copy in the post and you should receive this shortly"));
     }
 
     public void shouldSendAppointeeAppealWithdrawnNotification() throws NotificationClientException, IOException {
