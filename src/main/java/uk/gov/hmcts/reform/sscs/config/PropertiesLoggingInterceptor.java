3dc0bf0cf30d8799f86649cf197cd29ee071e034
Jarek Pierchala <jaroslaw.pierchala@hmcts.net>
2023-03-07 17:34:16 +0000

    syntax changes

--- src/main/java/uk/gov/hmcts/reform/sscs/config/PropertiesLoggingInterceptor.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/config/PropertiesLoggingInterceptor.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.sscs.config;
 
+import java.util.Collection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.event.ContextRefreshedEvent;
@@ -8,15 +9,13 @@ import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.MapPropertySource;
 import org.springframework.stereotype.Component;
 
-import java.util.Collection;
-
 @Component
 public class PropertiesLoggingInterceptor {
 
-    Logger LOGGER = LoggerFactory.getLogger(this.getClass().getName());
+    private static final Logger LOG = LoggerFactory.getLogger(PropertiesLoggingInterceptor.class);
 
     @EventListener
-    public void printPropertiesFromApplicationContext(ContextRefreshedEvent event){
+    public void printPropertiesFromApplicationContext(ContextRefreshedEvent event) {
         ConfigurableEnvironment env = (ConfigurableEnvironment) event.getApplicationContext().getEnvironment();
 
         env.getPropertySources().stream()
@@ -25,6 +24,10 @@ public class PropertiesLoggingInterceptor {
                 .flatMap(Collection::stream)
                 .distinct()
                 .sorted()
-                .forEach(key -> LOGGER.info("{} : {}", key, env.getProperty(key).substring(0, 4)));
+                .forEach(key -> {
+                    String propValue = env.getProperty(key);
+                    String propValueStripped = propValue.length() >= 5 ? propValue.substring(0, 4) : propValue;
+                    LOG.info("{} : {}", key, propValueStripped);
+                });
     }
 }
