e44ecef73ecd17e28650498ba7551fe51d7760a1
taleb <tech@benouaer.com>
2023-06-21 11:11:04 +0100

    SSCS-11054: upgrade springboot to v2.7.12

--- src/main/java/uk/gov/hmcts/reform/sscs/config/LegacyFlywayAutoConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/config/LegacyFlywayAutoConfiguration.java
@@ -1,15 +1,17 @@
 package uk.gov.hmcts.reform.sscs.config;
 
+import java.util.Set;
 import javax.sql.DataSource;
 import org.flywaydb.core.Flyway;
 import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationInitializer;
-import org.springframework.boot.autoconfigure.jdbc.JdbcOperationsDependsOnPostProcessor;
 import org.springframework.boot.jdbc.SchemaManagement;
 import org.springframework.boot.jdbc.SchemaManagementProvider;
+import org.springframework.boot.sql.init.dependency.DependsOnDatabaseInitializationDetector;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -51,11 +53,14 @@ public class LegacyFlywayAutoConfiguration {
     @ConditionalOnClass(JdbcOperations.class)
     @ConditionalOnBean(JdbcOperations.class)
     protected static class FlywayInitializerJdbcOperationsDependencyConfiguration
-            extends JdbcOperationsDependsOnPostProcessor {
+            implements DependsOnDatabaseInitializationDetector {
 
-        public FlywayInitializerJdbcOperationsDependencyConfiguration() {
-            super("flywayInitializer");
+        //TODO: check if registration is needed as described below
+        //Implementations should be registered in META-INF/spring.factories
+        // under the key org.springframework.boot.sql.init.dependency.DependsOnDatabaseInitializationDetector.
+        @Override
+        public Set<String> detect(ConfigurableListableBeanFactory beanFactory) {
+            return Set.of("flywayInitializer");
         }
-
     }
 }
