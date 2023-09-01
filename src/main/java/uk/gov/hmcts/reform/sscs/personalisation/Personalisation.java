33ceb54bfd4c181a9fe7bc6f32771c108d6f6bc6
Nicholas Hill <nicholas.hill@HMCTS.net>
2023-09-01 14:01:11 +0100

    fix logging SSCS-11080

--- src/main/java/uk/gov/hmcts/reform/sscs/personalisation/Personalisation.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/personalisation/Personalisation.java
@@ -313,11 +313,14 @@ public class Personalisation<E extends NotificationWrapper> {
                 personalisation.put(FINAL_DECISION_DATE, formattedDate);
             }
         }
-        log.info("Final decision date is {}", finalDecisionDate);
-        log.info("Final decision date is {}", finalDecisionDate);
-        log.info("Final decision date is {}", finalDecisionDate);
-        log.info("Final decision date is {}", finalDecisionDate);
-        log.info("Final decision date is {}", finalDecisionDate);
+        log.info("Final decision date is");
+        log.info("Final decision date is");
+        log.info("Final decision date is");
+        log.info("Final decision date is");
+        log.info("Final decision date is");
+        log.info("Final decision date is");
+        log.info("Final decision date is");
+        log.info("Final decision date is");
       
         boolean isGranted = isGranted(ccdResponse.getDwpState());
       
