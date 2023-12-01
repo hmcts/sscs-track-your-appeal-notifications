f8783a0145c78fd09abc3b1fb53138c25fda6996
Gemma Talbot <gemma.talbot@solirius.com>
2023-12-01 14:09:40 +0000

    Fix tests

--- src/main/java/uk/gov/hmcts/reform/sscs/service/NotificationService.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/service/NotificationService.java
@@ -25,7 +25,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
--- src/main/java/uk/gov/hmcts/reform/sscs/service/SendNotificationService.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/service/SendNotificationService.java
@@ -34,7 +34,6 @@ import java.util.Optional;
 import java.util.stream.Stream;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.ArrayUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
