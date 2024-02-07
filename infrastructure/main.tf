25752db9f44d633bfbd0c26628f4176cdc131bba
hashimalisolirius <HashimAli@HMCTS.NET>
2024-02-07 17:22:02 +0000

    update feature

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,13 +1,13 @@
 provider "azurerm" {
-  features {}
-}
-
-provider "azurerm" {
   features {
     resource_group {
       prevent_deletion_if_contains_resources = false
     }
   }
+}
+
+provider "azurerm" {
+  features {}
   skip_provider_registration = true
   alias                      = "postgres_network"
   subscription_id            = var.aks_subscription_id
