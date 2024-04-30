0b8a26284cb9712143d18773ebe2afd5260439b0
Gemma Talbot <gemma.talbot@solirius.com>
2024-04-30 15:18:41 +0100

    remove static from method in test

--- src/test/java/uk/gov/hmcts/reform/sscs/personalisation/PersonalisationTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/personalisation/PersonalisationTest.java
@@ -30,7 +30,6 @@ import static uk.gov.hmcts.reform.sscs.config.SubscriptionType.JOINT_PARTY;
 import static uk.gov.hmcts.reform.sscs.config.SubscriptionType.OTHER_PARTY;
 import static uk.gov.hmcts.reform.sscs.config.SubscriptionType.REPRESENTATIVE;
 import static uk.gov.hmcts.reform.sscs.domain.notify.NotificationEventType.*;
-import static uk.gov.hmcts.reform.sscs.personalisation.Personalisation.calculateMaxDwpResponseDays;
 
 import java.time.Instant;
 import java.time.LocalDate;
@@ -974,7 +973,7 @@ public class PersonalisationTest {
 
         Map result = personalisation.setEventData(new HashMap<>(), response, APPEAL_RECEIVED);
 
-        assertEquals(LocalDate.now().plusDays(calculateMaxDwpResponseDays(response.getBenefitCode())).format(DateTimeFormatter.ofPattern(RESPONSE_DATE_FORMAT)), result.get(APPEAL_RESPOND_DATE));
+        assertEquals(LocalDate.now().plusDays(personalisation.calculateMaxDwpResponseDays(response.getBenefitCode())).format(DateTimeFormatter.ofPattern(RESPONSE_DATE_FORMAT)), result.get(APPEAL_RESPOND_DATE));
     }
 
     @Test
